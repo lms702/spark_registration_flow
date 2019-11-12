e44ba241494a46a97351197b244f87db0e94d83d
Scott Mai <scottmai@usc.edu>
2019-11-11 21:53:49 -0800

    Organized files, added github repo link

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>
         Home page!
     </title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
@@ -27,10 +27,16 @@
                 <a class="btn btn-light link-btn" href="register.php" role="button">Register</a>
             </div>
         </div>
+        <div style="height: 20%"></div>
+        <div class="row">
+            <div class="col">
+                <p>Source code can be found on my GitHub repository <a href="https://github.com/lms702/spark_registration_flow">here</a>!</p>
+            </div>
+        </div>
     </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="jquery-3.4.1.js" type="text/javascript"></script>
+<script src="js/jquery-3.4.1.js" type="text/javascript"></script>
 </body>
 </html>
--- profile.php
+++ profile.php
@@ -30,15 +30,15 @@ $username = $user['username'];
     <title>
         Log in!
     </title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="jquery-3.4.1.js" type="text/javascript"></script>
+    <script src="js/jquery-3.4.1.js" type="text/javascript"></script>
 </head>
 <body>
 <div class="row" id="spacer">
     <div class="col col-2">
         <a href="index.php">
-            <img src="duck_icon.png" width="100%">
+            <img src="resources/duck_icon.png" width="100%">
         </a>
     </div>
     <div class="col col-10"></div>
@@ -58,6 +58,6 @@ $username = $user['username'];
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="login-script.js" type="text/javascript"></script>
+<script src="js/login-script.js" type="text/javascript"></script>
 </body>
 </html>
